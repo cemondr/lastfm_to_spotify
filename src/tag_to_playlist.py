22107ec14180a6db9c37d6eff3c93ba549c405b8
cemondr <cemconder@gmail.com>
2020-05-30 03:39:43 -0700

    defined class structure

---
+++ src/tag_to_playlist.py
@@ -0,0 +1,23 @@
+class tag_to_playlist:
+
+    def __init__(self):
+        pass
+
+    def connect_to_lastfm(self):
+        pass
+
+    def get_tagged_songs(self):
+        pass
+
+    def connect_to_spotify(self):
+        pass
+    
+    def is_playlist_there(self,tag):
+        pass
+    
+    def create_playlist(self):
+        pass
+
+    def add_to_playlist(self):
+        pass
+

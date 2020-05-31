478cf1c2e669f378af938e1ff2b22d2034203986
cemondr <cemconder@gmail.com>
2020-05-31 03:29:23 -0700

    lastfm connection

--- src/tag_to_playlist.py
+++ src/tag_to_playlist.py
@@ -1,16 +1,23 @@
 import spotipy
 import pylast
+import config
 
 class tag_to_playlist:
 
     def __init__(self):
-        pass
+        self.lastfm_api_key = config.lastfm_api_key
+        self.lastfm_api_secret = config.lastfm_api_secret
+        self.lastfm_username = config.lastfm_username
+        self.lastfm_password = pylast.md5(config.lastfm_password)
 
     def connect_to_lastfm(self):
-        pass
+        self.lastfm_network = pylast.LastFMNetwork(api_key=self.lastfm_api_key,api_secret=self.lastfm_api_secret,username=self.lastfm_username,password_hash=self.lastfm_password)
 
-    def get_tagged_songs(self):
-        pass
+    def get_tagged_songs(self,tag_name):
+        return self.lastfm_network.get_tag(tag_name)
+
+    def get_songs_from_tag(self,tag):
+        return tag.get_top_tracks()
 
     def connect_to_spotify(self):
         pass

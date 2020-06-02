728a59a31510d9ecb1369fcb0cfbc2a730e0a422
cemondr <cemconder@gmail.com>
2020-06-02 02:20:20 -0700

    spotify connection

--- src/tag_to_playlist.py
+++ src/tag_to_playlist.py
@@ -4,23 +4,28 @@ import config
 
 class tag_to_playlist:
 
+    DESCRIPTION = "Imported from last.fm tag"
+
     def __init__(self):
         self.lastfm_api_key = config.lastfm_api_key
         self.lastfm_api_secret = config.lastfm_api_secret
         self.lastfm_username = config.lastfm_username
         self.lastfm_password = pylast.md5(config.lastfm_password)
+        self.spotify_username = config.spotify_username
+        self.spotify_password = config.spotify_password
+        self.spotify_api_key = config.spotify_api_key
+        self.spotify_api_secret = config.spotify_api_secret
+        self.spotify_redirect_uri = config.spotify_redirect_uri
 
     def connect_to_lastfm(self):
         self.lastfm_network = pylast.LastFMNetwork(api_key=self.lastfm_api_key,api_secret=self.lastfm_api_secret,username=self.lastfm_username,password_hash=self.lastfm_password)
 
     def get_tagged_songs(self,tag_name):
-        return self.lastfm_network.get_tag(tag_name)
-
-    def get_songs_from_tag(self,tag):
-        return tag.get_top_tracks()
+        tag_content = lastfm_network.get_tag(tag_name)
+        return tag_content.get_top_tracks()
 
     def connect_to_spotify(self):
-        pass
+        self.spotify_token = spotipy.prompt_for_user_token(username=self.spotify_username,scope="playlist-modify-public playlist-modify-private",client_id=self.spotify_api_key,client_secret=self.spotify_api_secret,redirect_uri=self.spotify_redirect_uri)
     
     def is_playlist_there(self,tag):
         pass
